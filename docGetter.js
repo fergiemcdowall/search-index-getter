46762bbd6a2504f91217bb56a4274036402fdb55
Fergie McDowall <fergusmcdowall@gmail.com>
2016-02-12 15:48:44 +0100

    async constructor

--- docGetter.js
+++ docGetter.js
@@ -1,14 +1,32 @@
-module.exports = function (options) {
+module.exports = function (givenOptions, callback) {
   var docGetter = {}
-  docGetter.getDoc = function (docID, callback) {
-    options.indexes.get('DOCUMENT￮' + docID + '￮', function (err, value) {
-      if (err) {
-        return callback(err, null);
-      }
-      else {
-        return callback(null, value);
-      }
-    });
-  };
-  return docGetter;
+  getOptions(givenOptions, function(err, options) {
+    docGetter.getDoc = function (docID, callback) {
+      options.indexes.get('DOCUMENT￮' + docID + '￮', function (err, value) {
+        if (err) {
+          return callback(err, null);
+        }
+        else {
+          return callback(null, value);
+        }
+      })
+    }
+
+    return callback(err, docGetter);
+
+  })
+}
+
+var getOptions = function(givenOptions, callback) {
+  if (!givenOptions.indexes) {
+    levelup(givenOptions.indexPath || 'si', {
+      valueEncoding: 'json'
+    }, function(err, db) {
+      givenOptions.indexes = db
+      callback(null, givenOptions)
+    })
+  }
+  else {
+    callback(null, givenOptions)
+  }
 }
